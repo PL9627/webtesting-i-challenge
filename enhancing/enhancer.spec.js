deadf6b590f7323dcca48594975245e9b742efd0
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-07-14 17:12:39 -0700

    finished succeed test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,33 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+describe("Testing Jest", () => {
+  it("renders", () => {
+    expect(true).toBeTruthy();
+  });
+});
+
+describe("Enhancer Unit Tests", () => {
+    it("Succeeds to enhance", () => {
+        const {succeed} = enhancer
+
+        const newItem = succeed(item)
+
+        if (newItem.enhancement < 20) {
+            expect(newItem.enhancement).toBe(newItem.enhancement + 1)
+        } else {
+            expect(newItem.enhancement).toBe(newItem.enhancement)
+        }
+    })
+
+    it("Fails to enhance", () => {
+        
+    })
+
+    it("Repairs item", () => {
+        
+    })
+
+    it("Changes item name", () => {
+        
+    })
+})
