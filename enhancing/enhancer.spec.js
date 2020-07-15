3b40ecf7448ae40e1bab6d761ddcd4a9480c2e61
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-07-14 17:27:57 -0700

    finished get test and formatting

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -8,41 +8,49 @@ describe("Testing Jest", () => {
 });
 
 describe("Enhancer Unit Tests", () => {
-    it("Succeeds to enhance", () => {
-        const {succeed} = enhancer
+  it("Succeeds to enhance", () => {
+    const { succeed } = enhancer;
 
-        const newItem = succeed(item)
+    const newItem = succeed(item);
 
-        if (newItem.enhancement < 20) {
-            expect(newItem.enhancement).toBe(newItem.enhancement + 1)
-        } else {
-            expect(newItem.enhancement).toBe(newItem.enhancement)
-        }
-    })
+    if (newItem.enhancement < 20) {
+      expect(newItem.enhancement).toBe(newItem.enhancement + 1);
+    } else {
+      expect(newItem.enhancement).toBe(newItem.enhancement);
+    }
+  });
 
-    it("Fails to enhance", () => {
-        const {fail} = enhancer
+  it("Fails to enhance", () => {
+    const { fail } = enhancer;
 
-        const newItem = fail(item)
+    const newItem = fail(item);
 
-        if (newItem.enhancement < 15) {
-            expect(newItem.durability).toBe(newItem.durability - 5)
-        } else if (newItem.enhancement >= 15) {
-            expect(newItem.durability).toBe(newItem.durability - 10)
-        } else if (newItem.enhancement > 16) {
-            expect(newItem.durability).toBe(newItem.durability - 1)
-        }
-    })
+    if (newItem.enhancement < 15) {
+      expect(newItem.durability).toBe(newItem.durability - 5);
+    } else if (newItem.enhancement >= 15) {
+      expect(newItem.durability).toBe(newItem.durability - 10);
+    } else if (newItem.enhancement > 16) {
+      expect(newItem.durability).toBe(newItem.durability - 1);
+    }
+  });
 
-    it("Repairs item", () => {
-        const {repair} = enhancer
+  it("Repairs item", () => {
+    const { repair } = enhancer;
 
-        const newItem = repair(item)
+    const newItem = repair(item);
 
-       expect(newItem.durability).toBe(100)
-    })
+    expect(newItem.durability).toBe(100);
+  });
 
-    it("Changes item name", () => {
-        
-    })
-})
+  it("Changes item name", () => {
+    const { get } = enhancer;
+
+    const newItem = get(item);
+
+    if (newItem.enhancement === 0) {
+      expect(newItem.name).toBe(item.name);
+    } else if (newItem.enhancement > 0) {
+      expect(newItem.name).toBe(`[+${newItem.enhancement}] ${item.name}`);
+    }
+  });
+});
